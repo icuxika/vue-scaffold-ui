88537c945479c722da9a1743d3db246aa2622567
icuxika <icuxika@outlook.com>
2022-04-20 21:48:08 +0800

    docs: add some notes

--- src/views/components/DemoX.tsx
+++ src/views/components/DemoX.tsx
@@ -46,6 +46,8 @@ export const DemoX = defineComponent({
 		const codeVisible = ref(false);
 		const toggleCodeVisible = () =>
 			(codeVisible.value = !codeVisible.value);
+		// 将onClick事件设置在VButton上时，开发时没有问题，但是构建时vue-tsc会报错不通过，因为这个操作的相当于给
+		// VButton多添加了一个属性，但VButton作为一个自定义组件没有具备onClick,暂时通过套一层div来进行解决
 		return () => (
 			<div class={style.container}>
 				<h2>{(props.component as ComponentType).__sourceCodeTitle}</h2>
