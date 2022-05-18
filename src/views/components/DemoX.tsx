9731b8bcea4197ea5acb5336a4c5389d9fe8b001
icuxika <icuxika@outlook.com>
2022-05-18 20:11:40 +0800

    fix: remove <div> out of <v-button>
    https://github.com/vuejs/babel-plugin-jsx/issues/555

--- src/views/components/DemoX.tsx
+++ src/views/components/DemoX.tsx
@@ -45,18 +45,16 @@ export const DemoX = defineComponent({
 		const codeVisible = ref(false);
 		const toggleCodeVisible = () =>
 			(codeVisible.value = !codeVisible.value);
-		// 将onClick事件设置在VButton上时，开发时没有问题，但是构建时vue-tsc会报错不通过，因为这个操作的相当于给
-		// VButton多添加了一个属性，但VButton作为一个自定义组件没有具备onClick,暂时通过套一层div来进行解决
 		return () => (
 			<div class={style.container}>
 				<h2>{(props.component as ComponentType).__sourceCodeTitle}</h2>
 				<div class={style.component}>{h(props.component)}</div>
 				<div class={style.action}>
-					<div onClick={withModifiers(toggleCodeVisible, ["stop"])}>
-						<v-button>
-							{codeVisible.value ? "隐藏源代码" : "查看源代码"}
-						</v-button>
-					</div>
+					<v-button
+						onClick={withModifiers(toggleCodeVisible, ["stop"])}
+					>
+						{codeVisible.value ? "隐藏源代码" : "查看源代码"}
+					</v-button>
 				</div>
 				{codeVisible.value ? (
 					<div class={style.code}>
