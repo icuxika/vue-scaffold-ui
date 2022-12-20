f02c8af71c63640848d015f4b619cacd29c0023e
icuxika <icuxika@outlook.com>
2022-12-20 06:04:58 +0000

    feat: new home page

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -17,16 +17,9 @@ export const Home = defineComponent({
 		};
 		return () => (
 			<div class={style.container}>
-				<div class={style.banner}>
-					<div class={style.content}>
+				<div class={style.header}>
+					<div class={style.title}>
 						<h1>{t("title")}</h1>
-						<h2>{t("sub_title")}</h2>
-						<p class={style.actions}>
-							<a href={"#"}>GitHub</a>
-							<RouterLink to={"/docs"}>
-								{t("doc_link")}
-							</RouterLink>
-						</p>
 						<select v-model={locale.value}>
 							<option value={"zh"}>中文</option>
 							<option value={"en"}>English</option>
@@ -37,31 +30,13 @@ export const Home = defineComponent({
 						</select>
 					</div>
 				</div>
-				<div class={style.feature}>
-					<ul>
-						<li>
-							<svg class={style.icon}>
-								<use xlinkHref="#vue" />
-							</svg>
-							<h3>基于 Vue 3</h3>
-							<p>使用了 Vue 3 script setup</p>
-						</li>
-						<li>
-							<svg class={style.icon}>
-								<use xlinkHref="#ts" />
-							</svg>
-							<h3>基于 TypeScript</h3>
-							<p>源代码采用 TypeScript书写</p>
-						</li>
-						<li>
-							<svg class={style.icon}>
-								<use xlinkHref="#light" />
-							</svg>
-							<h3>代码易读</h3>
-							<p>每个组件的源代码都极其简洁</p>
-						</li>
-					</ul>
+				<div class={style.content}>
+					<div class={style.actions}>
+						<a href={"#"}>GitHub</a>
+						<RouterLink to={"/docs"}>{t("doc_link")}</RouterLink>
+					</div>
 				</div>
+				<div class={style.footer}></div>
 			</div>
 		);
 	},
