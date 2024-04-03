3643f4c15397d8519c669c4b2b1624c0f9a2f8ad
icuxika <icuxika@outlook.com>
2024-04-03 16:31:47 +0800

    style: add lint-staged

---
+++ .lintstagedrc.cjs
@@ -0,0 +1,18 @@
+const { ESLint } = require("eslint");
+
+const removeIgnoredFiles = async (files) => {
+    const eslint = new ESLint();
+    const ignoredFiles = await Promise.all(
+        files.map((file) => eslint.isPathIgnored(file))
+    );
+    console.log("ignoredFiles", ignoredFiles);
+    const filteredFiles = files.filter((_, i) => !ignoredFiles[i]);
+    return filteredFiles.join(" ");
+};
+
+module.exports = {
+    "*": async (files) => {
+        const filesToLint = await removeIgnoredFiles(files);
+        return [`eslint -c .eslintrc.json ${filesToLint} --max-warnings=0`];
+    },
+};
